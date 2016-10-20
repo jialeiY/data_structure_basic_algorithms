1c9ff33b3129755d299e16308ad152d05534a659
azraelyg <y-jialei@hotmail.com>
2016-10-19 23:50:05 -0700

    feat: add insertion sort

--- sorting_algorithm.py
+++ sorting_algorithm.py
@@ -19,3 +19,15 @@ def selection_sort(the_list):
         print the_list
     return the_list
 #selection_sort([11, 7, 12, 14, 19, 1, 6, 18, 8, 20])
+
+def insertion_sort(the_list):
+    for k in xrange(1,len(the_list)):
+        this_value=the_list[k]
+        i=k-1
+        while the_list[i]>this_value and i>=0:      
+            the_list[i+1]=the_list[i]
+            i-=1
+        the_list[i+1]=this_value        
+        print the_list
+    return the_list
+#insertion_sort([15, 5, 4, 18, 12, 19, 14, 10, 8, 20])
