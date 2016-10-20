3413811a32e0e39f0ea7c851431afce2ea7afac6
azraelyg <y-jialei@hotmail.com>
2016-10-19 23:42:03 -0700

    feat: add selection sort

--- sorting_algorithm.py
+++ sorting_algorithm.py
@@ -7,3 +7,15 @@ def bubble_sort(the_list):
     return the_list
 #bubble_sort([19, 1, 9, 7, 3, 10, 13, 15, 8, 12])
 
+def selection_sort(the_list):
+    for k in xrange(len(the_list)-1):
+        the_max=None
+        the_max_pos=None        
+        for i in xrange(len(the_list)-k):       
+            if the_list[i]>the_max:
+                the_max=the_list[i]
+                the_max_pos=i
+        the_list[-k-1],the_list[the_max_pos]=the_list[the_max_pos],the_list[-k-1]
+        print the_list
+    return the_list
+#selection_sort([11, 7, 12, 14, 19, 1, 6, 18, 8, 20])
